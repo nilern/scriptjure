7e588bd687d4fc8c31dffe01d6995285b4eaa725
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2014-09-27 14:39:43 +0300

    Add license information into project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject nilern.scriptjure "0.1.25"
+(defproject nilern.scriptjure "0.1.26"
   :description "a clojure DSL for generating javascript"
   :url "https://github.com/nilern/scriptjure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.6.0"]])
